c2b3b3f66efacc6a7c7a1f34acf8507c87021e4d
MJ-check <mj188163@163.com>
2020-08-20 09:41:35 +0800

    fix: mysql connection

--- app.js
+++ app.js
@@ -42,23 +42,7 @@ app.get("/*", (req, res, next) => {
 
 // =============== api ========================
 var connection = mysql.createConnection(config.mysql_config);
-connection.connect(err => {
-  if (err) {
-    console.error(err);
-  }
-});
-connection.on("error", err => {
-  if (err.code === "PROTOCOL_CONNECTION_LOST") {
-    console.log("DB Restart || " + err.message);
-    connection.connect(err => {
-      if (err) {
-        console.error(err);
-      }
-    });
-  } else {
-    console.error(err);
-  }
-});
+connection.connect();
 // 音乐相关API
 // 获取所有音乐信息
 app.get("/api/all_music", (req, res) => api.all_music(connection, req, res));
